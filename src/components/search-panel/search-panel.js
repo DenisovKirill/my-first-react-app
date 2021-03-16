da37d2d256cfa3da4e5198ad946cac6465239a1e
Kirill <kirill.denisov.khar@gmail.com>
2021-03-16 18:09:04 +0200

    'create'

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,17 @@
+import React from'react';
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <div>
+            <input 
+            className='form-control search-input'
+            type='text'
+            placeholder='Поиск по записям'
+            />
+
+        </div>
+    )
+}
+
+export default SearchPanel;
