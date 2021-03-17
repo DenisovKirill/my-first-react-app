66f980638dae226a557b52bd8075dad68d1b4752
Kirill <kirill.denisov.khar@gmail.com>
2021-03-17 20:04:53 +0200

    'props-state'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,12 @@ import PostAddForm from '../post-add-form'
 import './app.css'
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'fsfsffg'},
+        {label: 'This is so good', important: false, id: 'gfgmehklicrrj6'},
+        {label: 'I need a break...', important: false, id: 'mblmrpfaw'}
+    ]
     return (
         <div className='app'>
             <AppHeader/>
@@ -15,7 +21,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/'
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-// reportWebVitals();
